861e156b7c1371a487c453830673b2ea627ec4e1
Robin Wen <blockxyz@gmail.com>
2020-03-09 17:52:46 +0800

    Update crontab

--- monitor/chainx_blocks_crontab.sh
+++ monitor/chainx_blocks_crontab.sh
@@ -9,6 +9,4 @@
 # Date: 2019-6-25
 # Time: 16:29:30
 
-nohup /usr/bin/python /data/ExinPool/ChainX/monitor/chainx_blocks.py VALIDATOR ws://127.0.0.1:${YOUR_PORT} &
-nohup /usr/bin/python /data/ExinPool/ChainX/monitor/chainx_blocks.py BACKUP ws://127.0.0.1:${YOUR_PORT} &
-nohup /usr/bin/python /data/ExinPool/ChainX/monitor/chainx_blocks.py SYNC wss://exinpool-chainx.872369.com &
+nohup /usr/bin/python /data/ExinPool/ChainX/monitor/chainx_blocks.py &
